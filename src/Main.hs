26b55bf083ad744f445fb19c5132608124ad2f11
Anders Storm Hansen <ash@bruun-rasmussen.dk>
2016-11-27 13:42:52 +0100

    Add poc lookup from ip using MaxMinds free GeoLite2 db

--- src/Main.hs
+++ src/Main.hs
@@ -1,2 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.GeoIP2
+import Data.IP (IP(..))
+
 main = do
-  putStrLn "TBW"
+  db <- openGeoDB "geolite2-country.mmdb"
+  let ip = IPv4 "151.101.84.144"
+  print $ findGeoData db "da" ip
