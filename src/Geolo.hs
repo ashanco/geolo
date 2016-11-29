79a8c1db611fd39e32fdc96184a8aff663afb5e9
Anders Storm Hansen <ash@bruun-rasmussen.dk>
2016-11-30 00:37:20 +0100

    Serve location api using Servant

--- src/Geolo.hs
+++ src/Geolo.hs
@@ -1,9 +1,61 @@
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators     #-}
 
-import Data.GeoIP2
-import Data.IP (IP(..))
+module Main where
 
+import           Prelude                    ()
+import           Prelude.Compat
+
+import           Control.Monad.Trans.Either
+import           Data.Aeson.Compat
+import           Data.GeoIP2
+import           Data.IP
+import qualified Data.Text                  as T
+import           GHC.Generics
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+import           Servant
+import           System.IO
+import           Text.Read
+import           Web.HttpApiData
+
+type LocationAPI = "location" :> Capture "ip" IP :> Get '[JSON] Country
+
+data Country = Country
+  { countryCode :: String }
+  deriving (Generic, Show)
+
+instance FromText IP where
+  fromText text
+    | Right ip <- parseUrlPiece text = Just ip
+    | otherwise = Nothing
+
+instance ToJSON Country
+
+instance FromHttpApiData IP where
+  parseUrlPiece text
+    | Just ip <- readMaybe $ T.unpack text = Right ip
+    | otherwise = Left "Not parseable as IP"
+
+type Handler = EitherT ServantErr IO
+
+server :: GeoDB -> Server LocationAPI
+server = location
+  where location :: GeoDB -> IP -> Handler Country
+        location db ip = return Country { countryCode = T.unpack code} where
+          code
+            | Right GeoResult { geoCountryISO = Just ciso } <- findGeoData db "en" ip = ciso
+            | otherwise = "Unknown"
+
+locationAPI :: Proxy LocationAPI
+locationAPI = Proxy
+
+app :: GeoDB -> Application
+app db = serve locationAPI (server db)
+
+main :: IO ()
 main = do
   db <- openGeoDB "geolite2-country.mmdb"
-  let ip = IPv4 "151.101.84.144"
-  print $ findGeoData db "da" ip
+  run 8081 (app db)
